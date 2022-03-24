f626e260c2c51870a76e4f5e47f0d3cdf84d2038
jvasic <68637082+jvasic@users.noreply.github.com>
2022-03-24 14:52:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 from modules import Individual
 from utils import read_data, generate_data, get_courses_by_topic, course_to_user_matching_score, \
     person_to_person_matching_score
-
+from statistics import mean
 
 def main():
     data_path = "data.json"
@@ -22,19 +22,28 @@ def main():
         for course in providers[provider_id].available_courses:
             if course.topic == query_topic:
                 c2u_score = course_to_user_matching_score(course, query_user)
+
+                if len(course.course_rates.values()) == 0:
+                    average_rating = 1.0
+                else:
+                    average_rating = mean(course.course_rates.values()) / 10
+
+
                 print(f"[COURSE2USER] Provider -> {providers[provider_id].name} Course -> {course.name}\n"
-                      f"Course to User Score : {c2u_score}")
+                      f"Course to User Score : {c2u_score}\nAverage Rating : {average_rating}\n")
 
         print("------------------------------------------------------------------")
 
         for course, past_participants in providers[provider_id].past_participants.items():
             if course.topic == query_topic:
                 for past_user in past_participants:
+
                     p2p_score = person_to_person_matching_score(past_user, query_user)
                     print(
                         f"[USER2USER]Provider -> {providers[provider_id].name} Course -> {course.name}\n"
                         f"Participant : {past_user}\n"
                         f"User to User Score : {p2p_score}\n")
+                    
 
 
 if __name__ == '__main__':
